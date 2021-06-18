4e803fc996e9e949164df54a83ed7f6b35892f47
Sam Macbeth <smacbeth@duckduckgo.com>
2021-06-18 12:51:54 +0200

    Add test for issue #76.

---
+++ test/easylist.test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import { describe, before, it } from "mocha";
+
+import { blocklists } from "../src/lists";
+
+describe("easylist matching", () => {
+  const list = blocklists["easyprivacy"]();
+
+  before(() => list.fetch());
+
+  it("matches $third-party rules (#76)", async () => {
+    const result = await list.match(
+      "https://244631299-prod.rfksrv.com/rfk/js/12359-244631299/init.js"
+    );
+    expect(result.match).to.be.true;
+  });
+});
