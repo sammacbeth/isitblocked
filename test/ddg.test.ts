32daaa917af8929dae51d63c83d3ffa713cfa572
Sam Macbeth <sammacbeth@users.noreply.github.com>
2024-10-28 09:08:01 +0100

    Add tracker allowlist to DDG matching (#427)

---
+++ test/ddg.test.ts
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import { describe, before, it } from "mocha";
+import DuckDuckGoBlocking from "../src/ddg";
+
+
+describe('DDG Blocking Tests', () => {
+    const ddg = new DuckDuckGoBlocking()
+
+    before(async () => {
+        await ddg.fetch()
+    })
+
+    it('blocks a standard tracker', () => {
+        const result = ddg.match("https://www.google-analytics.com/analytics.js", "https://example.com/", 'script');
+        expect(result.match).to.be.true
+        expect(result.info.trackerAllowlist).to.be.false
+    })
+
+    it('all domains tracker allowlist', () => {
+        const result = ddg.match("https://assets.brightspot.abebooks.a2z.com/test", "https://example.com/", 'script');
+        expect(result.match).to.be.false
+        expect(result.info.trackerAllowlist).to.be.true
+    })
+
+    it('specific domain tracker allowlist', () => {
+        const result = ddg.match("https://static.addtoany.com/menu/page.js", "https://x22report.com/", 'script');
+        expect(result.match).to.be.false
+        expect(result.info.trackerAllowlist).to.be.true
+    })
+
+    it('specific domain tracker allowlist on different site', () => {
+        const result = ddg.match("https://static.addtoany.com/menu/page.js", "https://example.com/", 'script');
+        expect(result.match).to.be.true
+        expect(result.info.trackerAllowlist).to.be.false
+    })
+})
